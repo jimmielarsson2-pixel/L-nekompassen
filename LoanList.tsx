e74791cbf9f903c69cd8f2f02d8d8adca25d8c5d
jimmielarsson2-pixel <jimmie.larsson2@gmail.com>
2025-12-05 14:18:04 +0100

    Add LoanList component to display loan information

---
+++ LoanList.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export type Loan = {
+  bank: string;
+  type: string;
+  amount: number;
+  rate: number;
+  monthly: number;
+};
+
+interface LoanListProps {
+  loans: Loan[];
+}
+
+export default function LoanList({ loans }: LoanListProps) {
+  return (
+    <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+      <table style={{ width: "90%", borderCollapse: "collapse", marginTop: "20px" }}>
+        <thead>
+          <tr style={{ background: "#f4f4f4", textAlign: "left" }}>
+            <th>Bank</th>
+            <th>Typ</th>
+            <th>Belopp kvar</th>
+            <th>Ränta</th>
+            <th>Månad</th>
+          </tr>
+        </thead>
+        <tbody>
+          {loans.map((loan, index) => (
+            <tr key={index} style={{ borderBottom: "1px solid #e5e5e5" }}>
+              <td>{loan.bank}</td>
+              <td>{loan.type}</td>
+              <td>{loan.amount.toLocaleString("sv-SE")} kr</td>
+              <td>{loan.rate}%</td>
+              <td>{loan.monthly.toLocaleString("sv-SE")} kr</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
